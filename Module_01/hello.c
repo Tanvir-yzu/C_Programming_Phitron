ea7c79c2b1ee3bd4cfa2d38ba23d52610833bc1f
Tanvir-yzu <2020tanvir1971@gmail.com>
2024-03-31 01:46:06 +0800

    ass

--- Module_01/hello.c
+++ Module_01/hello.c
@@ -2,9 +2,11 @@
 
 int main(){
 int i;
-scanf("%d",&i);
-printf("%d",i);
-printf("\n");
-printf("tanvir history");
+////scanf("%d",&i);
+////printf("%d",i);
+////printf("%d");
+//int rahime = 100 , karima = 1010;
+ float rahime = 100.01 , karima = 1010;
+printf("%.2f %.2f ", rahime, karima);
 return 0 ; 
 }
